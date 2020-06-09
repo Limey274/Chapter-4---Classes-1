9323327ac69abcb76c69819d4427865f4635399f
Daniel <d.lalime7@gmail.com>
2020-06-09 07:26:30 -0700

    Add project files.

---
+++ SchoolTracker/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+namespace SchoolTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+
+            var adding = true;
+
+            while (adding)
+            {
+                var newStudent = new Student();
+
+                Console.Write("Student Name: ");
+                newStudent.Name = Console.ReadLine();
+
+                Console.Write("Student ID: ");
+                newStudent.StudentId = int.Parse(Console.ReadLine());
+
+                Console.Write("Student Credits: ");
+                newStudent.NumberOfCredits = int.Parse(Console.ReadLine());
+
+                Console.Write("Student GPA: ");
+                newStudent.Gpa = double.Parse(Console.ReadLine());
+
+
+                students.Add(newStudent);
+                Student.Count++;
+                //Console.WriteLine("Student Count: {0}", Student.Count);
+
+                Console.WriteLine("Add another? y/n");
+
+                if (Console.ReadLine() != "y")
+                    adding = false;
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine("Name: {0}, ID: {1}", student.Name, student.StudentId);
+            }
+        }
+    }    
+}
---
+++ SchoolTracker/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolTracker
+{
+    class Student
+    {
+        static public int Count = 0;
+
+        public string Name;
+        public int StudentId;
+        public int NumberOfCredits;
+        public double Gpa;
+
+        
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int studentid, int numberOfCredits, int gpa)
+        {
+            Name = name;
+            StudentId = studentid;
+            NumberOfCredits = numberOfCredits;
+            Gpa = gpa;
+        }
+
+    }
+}
